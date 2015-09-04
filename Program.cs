0d4794e191e30635efcdbf2ed707cb14b0ea8da3
Fidel <fidelm02@gmail.com>
2015-09-03 21:09:38 -0500

    Primeras clases con el que empece el projecto, falta depurarlas, las matrices aun estan en un estado 4x4

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistemas_Inteligentes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+            bool origen = false, destino = false;
+             nodesTree matrix_tree = new nodesTree();
+            do
+            {
+                option = menu();
+                switch (option)
+                {
+                    case 1:
+                        // Ingresa el original
+                        setNumbers(matrix_tree, true);
+                        origen = true;
+                        option = 10;
+                        break;
+                    case 2:
+                        // Ingresa meta
+                        setNumbers(matrix_tree, false);
+                        destino = true;
+                        option = 10;
+                        break;
+                    case 3:
+                        if (!destino && !origen)
+                            Console.WriteLine("\n\tNo haz marcado un estado inicial ni uno final.");
+                        if (!destino && origen)
+                            Console.WriteLine("\n\tNo haz marcado un estado final.");
+                        if (destino && !origen)
+                            Console.WriteLine("\n\tNo haz marcado un estado inicial.");
+                        if (destino && origen)
+                            matrix_tree.printRootGoal();
+                        Console.ReadLine();
+                        option = 10;
+                        break;
+                    case 4:
+                        // Cantidad de pasos
+                        option = 10;
+                        break;
+                    case 5:
+
+                        //Salir
+                        Console.WriteLine("\t ... Haz elegido salir ...");
+                        Console.ReadLine();
+                        option = 0;
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        Console.ReadLine();
+                        option = 10;
+                        break;
+                }
+            } while (option == 10);
+        }
+
+        static int menu()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\tOperaciones");
+            Console.WriteLine("\t  1.- Ingresar original");
+            Console.WriteLine("\t  2.- Ingresar meta");
+            Console.WriteLine("\t  3.- Mostrar origen y destino");
+            Console.WriteLine("\t  4.- Cantidad de pasos para llegar a meta");
+            Console.WriteLine("\t  5.- Salir");
+            Console.Write("\n\t  Opcion: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void setNumbers(nodesTree tree, bool root)
+        {
+            int[] n = new int[16];
+
+            Console.WriteLine("\n\tA  B  C  D");
+            Console.WriteLine(  "\tE  F  G  H");
+            Console.WriteLine(  "\tI  J  K  L");
+            Console.WriteLine(  "\tM  N  O  P\n");
+
+            Console.WriteLine("El valor 0 es tomado como EL ELEMENTO VACIO");
+            Console.WriteLine("Inserta las letras:");
+
+            char[] letras = {'A', 'B', 'C', 'D','E', 'F', 'G', 'H',
+                            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'};
+
+            for (int i = 0; i < letras.Count(); i++)
+            {
+                if (i % 4 == 0)
+                    Console.Write("\n");
+                Console.Write(letras[i]+": ");
+                n[i] = int.Parse(Console.ReadLine());
+            }
+
+            if (root)
+                tree.setRoot(n[0], n[1], n[2], n[3],
+                    n[4], n[5], n[6], n[7], n[8], n[9],
+                    n[10], n[11], n[12], n[13], n[14],
+                    n[15]);
+            else
+                tree.setGoal(  n[0], n[1], n[2], n[3],
+                    n [4], n [5], n [6], n [7], n [8], n [9],
+                    n[10], n[11], n[12], n[13], n[14], n[15]);
+        }
+    }
+}
---
+++ matrixState.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistemas_Inteligentes
+{
+    class matrixState {
+
+        private int depth;
+        public int[,] currentState;
+
+        public matrixState(int n0, int n1, int n2,
+            int n3, int n4, int n5, int n6, int n7,
+            int n8, int n9, int n10, int n11, int n12,
+            int n13, int n14, int n15)
+        {
+            currentState = new int[,]
+            {
+                {n0,  n1,  n2,  n3 },
+                {n4,  n5,  n6,  n7 },
+                {n8,  n9,  n10, n11},
+                {n12, n13, n14, n15}
+            };
+        }
+
+        public void printMatrix()
+        { 
+            for(int i = 0; i< 4; i++)
+            {
+                Console.Write("\n\t");
+                for(int j = 0; j<4; j++)
+                {
+                    int n = currentState[i,j];
+                    if (n != 0)
+                    {
+                        Console.Write(n);
+                        if (j != 3)
+                            Console.Write("\t");
+                    }
+                    else
+                        Console.Write("-\t");
+                }
+            }
+        }
+
+        public void setDepth(int depth) { this.depth = depth; }
+
+        public int getDepth() { return depth; }
+
+        public bool comparison(matrixState other)
+        {
+            for (int i = 0; i < 4; i++)
+                for (int j = 0; j < 4; j++)
+                    if (currentState[i, j] != other.currentState[i, j])
+                        return false;
+            return true;
+        }
+    }
+}
