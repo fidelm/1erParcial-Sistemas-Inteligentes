7790c9d8b78c599f7803ac4d052769336e90b172
Fidel <fidelm02@gmail.com>
2015-09-05 22:57:52 -0500

    actualizacion a una version mas reciente de los archivos y ya se agrego la clase que se enfoca unicamente por amplitud

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace Sistemas_Inteligentes
         {
             int option;
             bool origen = false, destino = false;
-             nodesTree matrix_tree = new nodesTree();
+            nodesTree matrix_tree = new nodesTree();
             do
             {
                 option = menu();
@@ -20,30 +21,32 @@ namespace Sistemas_Inteligentes
                 {
                     case 1:
                         // Ingresa el original
-                        setNumbers(matrix_tree, true);
+                        //setNumbers(matrix_tree, true);
+                        matrix_tree.setRoot(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
                         origen = true;
                         option = 10;
                         break;
                     case 2:
                         // Ingresa meta
-                        setNumbers(matrix_tree, false);
+                        //setNumbers(matrix_tree, false);
+                        matrix_tree.setGoal(1, 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
                         destino = true;
                         option = 10;
                         break;
                     case 3:
-                        if (!destino && !origen)
-                            Console.WriteLine("\n\tNo haz marcado un estado inicial ni uno final.");
-                        if (!destino && origen)
-                            Console.WriteLine("\n\tNo haz marcado un estado final.");
-                        if (destino && !origen)
-                            Console.WriteLine("\n\tNo haz marcado un estado inicial.");
-                        if (destino && origen)
+                        if (validacion(destino, origen))
                             matrix_tree.printRootGoal();
                         Console.ReadLine();
                         option = 10;
                         break;
                     case 4:
-                        // Cantidad de pasos
+                        // Solucion
+                        if (validacion(destino, origen))
+                        {
+                            matrixState solucion = new matrixState();
+                            solucion = matrix_solution(matrix_tree.root, matrix_tree.goal);
+                            printSolution(solucion, matrix_tree.goal);
+                        }
                         option = 10;
                         break;
                     case 5:
@@ -69,7 +72,7 @@ namespace Sistemas_Inteligentes
             Console.WriteLine("\t  1.- Ingresar original");
             Console.WriteLine("\t  2.- Ingresar meta");
             Console.WriteLine("\t  3.- Mostrar origen y destino");
-            Console.WriteLine("\t  4.- Cantidad de pasos para llegar a meta");
+            Console.WriteLine("\t  4.- Solucion");
             Console.WriteLine("\t  5.- Salir");
             Console.Write("\n\t  Opcion: ");
             return int.Parse(Console.ReadLine());
@@ -80,9 +83,9 @@ namespace Sistemas_Inteligentes
             int[] n = new int[16];
 
             Console.WriteLine("\n\tA  B  C  D");
-            Console.WriteLine(  "\tE  F  G  H");
-            Console.WriteLine(  "\tI  J  K  L");
-            Console.WriteLine(  "\tM  N  O  P\n");
+            Console.WriteLine("\tE  F  G  H");
+            Console.WriteLine("\tI  J  K  L");
+            Console.WriteLine("\tM  N  O  P\n");
 
             Console.WriteLine("El valor 0 es tomado como EL ELEMENTO VACIO");
             Console.WriteLine("Inserta las letras:");
@@ -94,7 +97,7 @@ namespace Sistemas_Inteligentes
             {
                 if (i % 4 == 0)
                     Console.Write("\n");
-                Console.Write(letras[i]+": ");
+                Console.Write(letras[i] + ": ");
                 n[i] = int.Parse(Console.ReadLine());
             }
 
@@ -104,9 +107,153 @@ namespace Sistemas_Inteligentes
                     n[10], n[11], n[12], n[13], n[14],
                     n[15]);
             else
-                tree.setGoal(  n[0], n[1], n[2], n[3],
-                    n [4], n [5], n [6], n [7], n [8], n [9],
+                tree.setGoal(n[0], n[1], n[2], n[3],
+                    n[4], n[5], n[6], n[7], n[8], n[9],
                     n[10], n[11], n[12], n[13], n[14], n[15]);
+        }
+
+        static bool validacion(bool destino, bool origen)
+        {
+            if (!destino && !origen)
+                Console.WriteLine("\n\tNo haz marcado un estado inicial ni uno final.");
+            if (!destino && origen)
+                Console.WriteLine("\n\tNo haz marcado un estado final.");
+            if (destino && !origen)
+                Console.WriteLine("\n\tNo haz marcado un estado inicial.");
+            if (destino && origen)
+                return true;
+            return false;
+        }
+
+        static matrixState matrix_solution(matrixState root, matrixState goal)
+        {
+            Queue cola = new Queue();
+            bool encontrado = false;
+            // agregar elemento cola.Enqueue(elemento)
+            // retirar elemento de la cola cola.Dequeue();
+            matrixState currentState = new matrixState();
+            root.setValues(currentState);
+            cola.Enqueue(currentState);
+
+            while (!encontrado)
+            {
+                int x, y;
+                bool up, down, right, left;
+
+                x = y = 0;
+                up = down = right = left = true;
+
+                // Detect which element is the null one.
+                for (int raw = 0; raw < 4; raw++)
+                    for (int column = 0; column < 4; column++)
+                        if (currentState.currentState[raw, column] == 0)
+                        {
+                            x = column; y = raw;
+                            break;
+                        }
+
+                // Validate movements
+                if (x == 0) left = false;
+                if (x == 3) right = false;
+                if (y == 0) up = false;
+                if (y == 3) down = false;
+
+                if (up)
+                {
+                    matrixState newState = specific_movement(currentState, 0);
+                    newState.previosSteps.Add(0);
+                    newState.setDepth(newState.getDepth() + 1);
+                    cola.Enqueue(newState);
+                }
+                if (down)
+                {
+                    matrixState newState = specific_movement(currentState, 1);
+                    newState.previosSteps.Add(1);
+                    newState.setDepth(newState.getDepth() + 1);
+                    cola.Enqueue(newState);
+                }
+                if (left)
+                {
+                    matrixState newState = specific_movement(currentState, 2);
+                    newState.previosSteps.Add(2);
+                    newState.setDepth(newState.getDepth() + 1);
+                    cola.Enqueue(newState);
+                }
+                if (right)
+                {
+                    matrixState newState = specific_movement(currentState, 3);
+                    newState.previosSteps.Add(3);
+                    newState.setDepth(newState.getDepth() + 1);
+                    cola.Enqueue(newState);
+                }
+                currentState = (matrixState)cola.Dequeue();
+                encontrado = currentState.comparison(goal);
+            }
+            return currentState;
+        }
+
+        static void printSolution(matrixState m_solution, matrixState root)
+        {
+            ArrayList steps = m_solution.previosSteps;
+            matrixState current = new matrixState();
+            root.setValues(current);
+            current.printMatrix();
+            Console.WriteLine("\n");
+            foreach (int element in steps)
+            {
+                /*
+                Console.WriteLine(element);
+                current = specific_movement(current, element);
+                current.printMatrix();
+                Console.WriteLine("\n");
+                Console.ReadLine();
+                 * */
+                Console.WriteLine(element);
+            }
+            Console.ReadLine();
+
+        }
+
+        static matrixState specific_movement(matrixState currentState, int movement)
+        {
+            matrixState newState = new matrixState();
+            currentState.setValues(newState);
+            int x = 0, y = 0;
+            // Detect which element is the null one.
+            for (int raw = 0; raw < 4; raw++)
+                for (int column = 0; column < 4; column++)
+                    if (currentState.currentState[raw, column] == 0)
+                    {
+                        x = column; y = raw;
+                        break;
+                    }
+            switch (movement)
+            {
+                case 0:
+                    // Up
+                    newState.currentState[y, x] = newState.currentState[y - 1, x];
+                    newState.currentState[y - 1, x] = 0;
+                    break;
+                case 1:
+                    // Down
+                    newState.currentState[y, x] = newState.currentState[y + 1, x];
+                    newState.currentState[y + 1, x] = 0;
+                    break;
+                case 2:
+                    // Left
+                    newState.currentState[y, x] = newState.currentState[y, x - 1];
+                    newState.currentState[y, x - 1] = 0;
+                    break;
+                case 3:
+                    // Right
+                    newState.currentState[y, x] = newState.currentState[y, x + 1];
+                    newState.currentState[y, x + 1] = 0;
+                    break;
+                default:
+                    // Invalid movement
+                    break;
+            }
+            return newState;
         }
     }
 }
--- matrixState.cs
+++ matrixState.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,34 +7,54 @@ using System.Threading.Tasks;
 
 namespace Sistemas_Inteligentes
 {
-    class matrixState {
+    class matrixState
+    {
 
         private int depth;
         public int[,] currentState;
+        public ArrayList previosSteps;
+        /// <summary>
+        /// Steps:
+        ///     0 Up
+        ///     1 Down
+        ///     2 Left
+        ///     3 Right
+        /// </summary>
+
+
+        public matrixState()
+        {
+            previosSteps = new ArrayList();
+            depth = 0;
+            currentState = new int[3, 3];
+        }
 
         public matrixState(int n0, int n1, int n2, int n3, 
-            int n4, int n5, int n6, int n7,int n8)
+            int n4, int n5, int n6, int n7, int n8)
         {
             currentState = new int[,]
             {
-                {n0, n1, n2},
-                {n3, n4, n5},
-                {n6, n7, n8}
+                {n0,  n1,  n2},
+                {n3,  n4,  n5},
+                {n6,  n7,  n8}
             };
+
+            previosSteps = new ArrayList();
+            depth = 0;
         }
 
         public void printMatrix()
-        { 
-            for(int i = 0; i< 3; i++)
+        {
+            for (int i = 0; i < 3; i++)
             {
                 Console.Write("\n\t");
-                for(int j = 0; j<3; j++)
+                for (int j = 0; j < 3; j++)
                 {
-                    int n = currentState[i,j];
+                    int n = currentState[i, j];
                     if (n != 0)
                     {
                         Console.Write(n);
-                        if (j != 3)
+                        if (j != 2)
                             Console.Write("\t");
                     }
                     else
@@ -53,6 +74,15 @@ namespace Sistemas_Inteligentes
                     if (currentState[i, j] != other.currentState[i, j])
                         return false;
             return true;
+        }
+
+        public void setValues(matrixState external)
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    external.currentState[i, j] = currentState[i, j];
+            external.setDepth(depth);
+            external.previosSteps = previosSteps;
         }
     }
 }
